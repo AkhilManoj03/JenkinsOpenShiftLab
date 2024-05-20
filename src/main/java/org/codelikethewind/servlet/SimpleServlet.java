1c3edbbe430193b74c956a42e344b59515efde14
Akhil Manoj <manoja2@vcu.edu>
2024-05-20 16:14:58 -0700

    Changed Hello World

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello Akhil Manoj" + "</h1>");
+		out.println("<h1>" + "Hello Akhil" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
